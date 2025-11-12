b08c7d6f1ad4494f8be154b9a820fbefb0ea1e62
kstrong03 <kat.strong2003@gmail.com>
2025-11-12 13:26:12 -0500

    added code to Card, Deck and Prospector scripts

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -1,16 +1,28 @@
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;      // This will be used later in the project
+using UnityEngine.UI;                   // This will be used later in the project
 
 public class Prospector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    static public Prospector S;
+
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+
+    [Header("Set Dynamically")]
+    public Deck deck;
+
+    void Awake()
     {
-        
+        S = this; // Set up a Singleton for Prospector
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        deck = GetComponent<Deck>(); // Get the Deck
+        deck.InitDeck(deckXML.text); // Pass DeckXML to it
     }
 }
